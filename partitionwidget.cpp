c78ae505577804cf4edfba556c7c53b2a7816132
P1erreGaultier <pit.gaultier@free.fr>
2016-03-16 17:58:50 +0100

    Fin de la partoch

--- partitionwidget.cpp
+++ partitionwidget.cpp
@@ -24,37 +24,16 @@ void PartitionWidget::paintEvent(QPaintEvent *event)
         QPoint p;
         QVector<QPoint> V(12);
 
-        QString clef("𝄚𝄞𝄚");
-        QString quarter_note("♩");
+        QString clef = QString::fromUtf8("𝄞");
+        QString quarter_note = QString::fromUtf8("♩");
 
-        //Creation de la grille de la partition
         painter.setPen(QPen(Qt::black, 5, Qt::SolidLine, Qt::RoundCap));
-        //horizontal
-        painter.drawLine(0, 50,1250,50);
-        painter.drawLine(0, 75,1250,75);
-        painter.drawLine(0, 100,1250,100);
-        painter.drawLine(0, 125,1250,125);
-        painter.drawLine(0, 150,1250,150);
-
-        //vertical
-        painter.drawLine(50, 50,50,150);
-        painter.drawLine(450, 50,450,150);
-        painter.drawLine(850, 50,850,150);
-        painter.drawLine(1250,50,1250,150);
-
-
         QFont font = painter.font();
         font.setPointSize(font.pointSize() * 8);
         painter.setFont(font);
-        painter.drawText(50, 150, clef);
-
-        //font.setPointSize(font.pointSize() / 2);
-        //painter.setFont(font);
+        painter.drawText(-10, 130, clef);
 
 
-
-
-        //Creation des points representant les notes dans le vector V
         QFile file(":/partoch1.txt");
         if(!file.open(QIODevice::ReadOnly)) {
             QMessageBox::information(0, "error", file.errorString());
@@ -64,16 +43,32 @@ void PartitionWidget::paintEvent(QPaintEvent *event)
 
         for (int i = 0; i < V.size(); i++) {
              QString line = in.readLine();
-             p.setX((1+i)*100);
-             p.setY(line.toInt()*12.5);
+             p.setX((1+i)*100-50);
+             p.setY(line.toInt()*12.5-3);
              V[i] = p;
             }
          file.close();
 
-        //Tracé des points
+        //Tracé des notes
         //painter.setPen(QPen(Qt::blue, 12, Qt::SolidLine, Qt::RoundCap));
         for (int i = 0; i < V.size(); ++i) {
-            //painter.drawPoint(V.at(i));
             painter.drawText(V.at(i), quarter_note);
         }
+
+
+        //Creation de la grille de la partition
+        painter.setPen(QPen(Qt::black, 3, Qt::SolidLine, Qt::RoundCap));
+        //horizontal
+        painter.drawLine(0, 50,50 + V.size()*100,50);
+        painter.drawLine(0, 75,50 + V.size()*100,75);
+        painter.drawLine(0, 100,50 + V.size()*100,100);
+        painter.drawLine(0, 125,50 + V.size()*100,125);
+        painter.drawLine(0, 150,50 + V.size()*100,150);
+
+        //vertical
+        for (int i = 0; i <= V.size()/4; ++i) {
+            painter.drawLine(i*400+50, 50,i*400+50,150);
+        }
+
     }
+
